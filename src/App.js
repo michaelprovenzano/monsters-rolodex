bbc0af5d03e62c9711764de139e92b4118b3f00d
Michael Provenzano <mikepmusic@gmail.com>
2020-04-22 16:06:07 -0400

    Fixed search box functionality

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     return (
       <div className='App'>
         <h1>Monsters Rolodex</h1>
-        <SearchBox placeholder='search monsters' onChange={this.handleChange} />
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange} />
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
